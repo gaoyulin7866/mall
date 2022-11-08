d89766d72fd593e0038671035acd557013aa14a7
高钰林 <gaoyulin@xiaomi.com>
2022-11-08 20:08:54 +0800

    线程池配置

--- shopping-server/src/main/java/com/gyl/shopping/config/ThreadPoolConfig.java
+++ shopping-server/src/main/java/com/gyl/shopping/config/ThreadPoolConfig.java
@@ -1,6 +1,10 @@
 package com.gyl.shopping.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
 
 /**
  * @Author: gyl
@@ -10,5 +14,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ThreadPoolConfig {
 
-//    private
+    @Value("${thread.corePoolSize}")
+    private Integer corePoolSize;
+
+    @Value("${thread.maximumPoolSize}")
+    private Integer maximumPoolSize;
+
+    @Value("${thread.keepAliveTime}")
+    private Integer keepAliveTime;
+
+    private final TimeUnit timeUnit = TimeUnit.SECONDS;
+
+    private final BlockingQueue<Runnable> blockingQueue = new LinkedBlockingDeque<Runnable>();
+
+    @Bean(name = "executorService")
+    public ExecutorService createThread(){
+        return new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, timeUnit, blockingQueue);
+    }
 }
